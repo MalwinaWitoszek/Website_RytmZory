f0869feec8f6a3c7ec6c08b6d7e7acb9fc862713
Manislawa <malwina.witoszek82@gmail.com>
2018-10-17 23:05:14 +0200

    gallery section with quicksand and lightcase plugin

--- gulpfile.js
+++ gulpfile.js
@@ -2,39 +2,43 @@
 // INSTALACJA PLUGINÓW
 // npm install --save-dev gulp gulp-watch browser-sync gulp-sass gulp-autoprefixer gulp-plumber del gulp-useref gulp-if gulp-uglify gulp-imagemin run-sequence
 
-
-
 //  Variables
 
 var gulp = require("gulp");
-var sass = require("gulp-sass");                    //kompilacja scss --> css i minifikacja csss
+var sass = require("gulp-sass"); //kompilacja scss --> css i minifikacja csss
 // var	autoprefixer = require('autoprefixer');    // dodanie wendor prefiksów
-var	autoprefixer = require('gulp-autoprefixer');    // dodanie wendor prefiksów
-var	watch = require('gulp-watch');                      // nasłuchiwanie zmian w plikach
-var plumber = require('gulp-plumber');              // zapobiega przerywaniu zadań - obsługa błędów
+var autoprefixer = require("gulp-autoprefixer"); // dodanie wendor prefiksów
+var watch = require("gulp-watch"); // nasłuchiwanie zmian w plikach
+var plumber = require("gulp-plumber"); // zapobiega przerywaniu zadań - obsługa błędów
 var del = require("del");
-var useref = require('gulp-useref');        // konkatenacja plików js bez minifikacji
-var gulpif = require('gulp-if');            // sprawdzanie warunków
-var uglify = require('gulp-uglify');        // minifikacja plików js
-var imagemin = require('gulp-imagemin');    // kompresja obrazów
-var runSequence = require('run-sequence');    // kompresja obrazów
-var browserSync = require('browser-sync').create(); // przeładowanie przeglądarki
-
+var useref = require("gulp-useref"); // konkatenacja plików js bez minifikacji
+var gulpif = require("gulp-if"); // sprawdzanie warunków
+var uglify = require("gulp-uglify"); // minifikacja plików js
+var imagemin = require("gulp-imagemin"); // kompresja obrazów
+var runSequence = require("run-sequence"); // kompresja obrazów
+var browserSync = require("browser-sync").create(); // przeładowanie przeglądarki
 
 // styleSheets
 
-gulp.task('styles', function() {
-    return gulp.src("src/sass/main.scss")
-    .pipe(plumber())  //  zapobiega przerywaniu zadań - obsługa błędów
-    .pipe(sass.sync({  //   kompilacja SCSS → CSS
-        outputStyle: "expanded"          // możliwości: nested, expanded, compact, compressed
-    }))
-    .pipe(autoprefixer({   // dodanie wendor prefiksów
-        browsers: ['last 2 versions'],
+gulp.task("styles", function() {
+  return gulp
+    .src("src/sass/main.scss")
+    .pipe(plumber()) //  zapobiega przerywaniu zadań - obsługa błędów
+    .pipe(
+      sass.sync({
+        //   kompilacja SCSS → CSS
+        outputStyle: "expanded" // możliwości: nested, expanded, compact, compressed
+      })
+    )
+    .pipe(
+      autoprefixer({
+        // dodanie wendor prefiksów
+        browsers: ["last 2 versions"],
         grid: true
-    }))
-    .pipe(gulp.dest('src/css'))
-    .pipe(browserSync.stream())	// przeładowanie przeglądarki
+      })
+    )
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream()); // przeładowanie przeglądarki
 });
 // gulp.task('styles', function() {
 //     return gulp.src("src/sass/main.scss")
@@ -47,17 +51,16 @@ gulp.task('styles', function() {
 //     .pipe(browserSync.stream())	// przeładowanie przeglądarki
 // });
 
-
 //  JavaScript
 
-
 gulp.task("scripts", function() {
-    return gulp.src("src/*.html")
+  return gulp
+    .src("src/*.html")
     .pipe(useref())
-    .pipe(gulpif("*.js",uglify()))      // jeżeli plik ma rozszerzenie js, to wywołujemy uglify
+    .pipe(gulpif("*.js", uglify())) // jeżeli plik ma rozszerzenie js, to wywołujemy uglify
     .pipe(gulp.dest("dist/"));
-    })
- // w index html nalezy zgodnie ze wzorem podac:
+});
+// w index html nalezy zgodnie ze wzorem podac:
 // przykład do USEREF - konkatenacja plików, należy dodać:
 // <html>
 //     <head>
@@ -74,66 +77,60 @@ gulp.task("scripts", function() {
 //     </body>
 // </html>
 
-
 //  Images
 
 gulp.task("images", function() {
-    return gulp.src("dist/img/*", {      // tworzy obiekt aby pobrac pliki z bazy dist
-        base: "dist/"
-        })
+  return gulp
+    .src("dist/img/*", {
+      // tworzy obiekt aby pobrac pliki z bazy dist
+      base: "dist/"
+    })
     .pipe(imagemin())
     .pipe(gulp.dest("dist/"));
-    });
-
+});
 
 // Automatyzacja
 
-gulp.task('watch', function() {	                // nasłuchiwanie zmian w plikach
-    gulp.watch('src/sass/**/*.scss', ['styles']);
-    gulp.watch(["src/*.html", "src/**/*.js"], browserSync.reload);
+gulp.task("watch", function() {
+  // nasłuchiwanie zmian w plikach
+  gulp.watch("src/sass/**/*.scss", ["styles"]);
+  gulp.watch(["src/*.html", "src/**/*.js"], browserSync.reload);
 });
-    // gulp.watch(["src/*.scripts", "src/**/*.js"]).on('change', browserSync.reload);
+// gulp.watch(["src/*.scripts", "src/**/*.js"]).on('change', browserSync.reload);
 
-
-gulp.task('clean', function() {	                // usunięcie katalogu dist - z wersją dystrybucyjna projektu
-    return del("dist/")
+gulp.task("clean", function() {
+  // usunięcie katalogu dist - z wersją dystrybucyjna projektu
+  return del("dist/");
 });
 
 gulp.task("copy", function() {
-    return gulp.src(["src/css/**/*.css", "src/img/*"], {
-    base: "src"
+  return gulp
+    .src(["src/css/**/*.css", "src/img/*"], {
+      base: "src"
     })
     .pipe(gulp.dest("dist/"));
-    })
-
-
-gulp.task('server-sync', function() {     // stworzenie serwera w katalogu src
-    browserSync.init({
-    server: {
-        baseDir: "src/"
-        }
-    });
 });
 
+gulp.task("server-sync", function() {
+  // stworzenie serwera w katalogu src
+  browserSync.init({
+    server: {
+      baseDir: "src/"
+    }
 
-
+  });
+});
 
 // wykonywanie sekwencji zadań - wywoanie gulp build
 // Jeżeli wyjdzie GULP 4 to zamienić to na gulp.series !
 
 gulp.task("build", function() {
-    runSequence("clean", "scripts", "copy", "images");
-    })
-
+  runSequence("clean", "scripts", "copy", "images");
+});
 
 //  Zadania domyślne
 
-gulp.task("default", ["styles", "server-sync", "watch"]);   //wywołanie w terminalu: gulp
-
-
-
-
-
+gulp.task("default", ["styles", "server-sync", "watch"]); //wywołanie w terminalu: gulp
 
 //układ katalogów:
 // Project /src
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,4 +1,4 @@
-(function($) {
+(function ($) {
   "use strict";
 
   // PRE LOADER
@@ -7,11 +7,11 @@
   // });
 
   // MENU
-  $(".navbar-collapse a").on("click", function() {
+  $(".navbar-collapse a").on("click", function () {
     $(".navbar-collapse").collapse("hide");
   });
 
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(".navbar").offset().top > 50) {
       $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
@@ -27,33 +27,261 @@
   // Owl Carousel
   var owl = $(".owl-carousel");
   owl.owlCarousel({
-    autoplay:true,
+    autoplay: true,
     autoplayTimeout: 5000,
     // autoplayHoverPause: true,
     autoplaySpeed: 1000,
     dots: true,
-    margin:40,
-    responsive:{
-        0:{
-            items:1
-        },
-        768:{
-            items:2
-        },
-        1199:{
-            items:3
-        },
-        1700:{
-          items:4
+    margin: 40,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      1199: {
+        items: 3
+      },
+      1700: {
+        items: 4
       }
     }
-});
-// owl.on('mousewheel', '.owl-stage', function (e) {
-//     if (e.deltaY>0) {
-//         owl.trigger('next.owl');
-//     } else {
-//         owl.trigger('prev.owl');
-//     }
-//     e.preventDefault();
-// });
+  });
+
+
+/* ==================================================
+   GALLERY
+================================================== */
+
+  //filtering
+  var $filterType = $('.filter li.active a').attr('class');
+  var $holder = $('ul.holderfilter');
+  var $data = $holder.clone();
+
+  $('.filter li a').click(function (e) {
+
+    $('.filter li').removeClass('active');
+
+    var $filterType = $(this).attr('class');
+
+    $(this).parent().addClass('active');
+
+    if ($filterType == 'test1') {
+
+      var $filteredData = $data.find('li');
+    } else {
+
+      var $filteredData = $data.find('li[data-type=' + $filterType + ']');
+    }
+
+    $holder.quicksand($filteredData, {
+      duration: 1000,
+      easing: 'easeInOutQuad'
+    });
+    return false;
+  });
+  // lightcase - gallery-zoom
+  $('a[data-rel^=lightcase]').lightcase({
+    swipe: true,
+    fullscreenModeForMobile: true,
+    transition: 'scrollHorizontal',
+    speedIn: 1100,
+    speedOut: 900,
+    showSequenceInfo: false,
+    maxHeight: 600
+  });
+
+
+
+  // CONTACT FORM
+  $('.myform').on('submit', function () {
+
+    // Add text 'loading...' right after clicking on the submit button.
+    $('.output_message').text('Loading...');
+
+    var form = $(this);
+    $.ajax({
+      url: form.attr('action'),
+      method: form.attr('method'),
+      data: form.serialize(),
+      success: function (result) {
+        if (result == 'success') {
+          $('.output_message').text('Message Sent!');
+        } else {
+          $('.output_message').text('Error Sending email!');
+        }
+      }
+    });
+
+    // Prevents default submission of the form after clicking on the submit button.
+    return false;
+  });
+
+  //   var form = $('.contact__form'),
+  //   message = $('.contact__msg'),
+  //   form_data;
+  // // Success function
+  // function done_func(response) {
+  //   message.fadeIn().removeClass('alert-danger').addClass('alert-success');
+  //   message.text(response);
+  //   setTimeout(function () {
+  //       message.fadeOut();
+  //   }, 2000);
+  //   form.find('input:not([type="submit"]), textarea').val('');
+  // }
+  // // fail function
+  // function fail_func(data) {
+  //   message.fadeIn().removeClass('alert-success').addClass('alert-success');
+  //   message.text(data.responseText);
+  //   setTimeout(function () {
+  //       message.fadeOut();
+  //   }, 2000);
+  // }
+
+  // form.submit(function (e) {
+  //   e.preventDefault();
+  //   form_data = $(this).serialize();
+  //   $.ajax({
+  //       type: 'POST',
+  //       url: form.attr('action'),
+  //       data: form_data
+  //   })
+  //   .done(done_func)
+  //   .fail(fail_func);
+  // });
+
+  //   // Quicksand - filtering
+  // $("#content").quicksand($("#data > li"),
+  // {
+  //   // all the parameters have sensible defaults
+  //   // and in most cases can be optional
+  //   duration: 1000,
+  //   easing: "swing",
+  //   attribute: "data-id",
+  // }
+  // );
+
+  // <section id="projects">
+
+  //                               <ul id="thumbs">
+
+  //                                               <!-- Item Project and Filter Name -->
+  //                                   <li class="item-thumbs span3 design">
+  //                                       <!-- Fancybox - Gallery Enabled - Title - Full Image -->
+  //                                       <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="The City" href="_include/img/work/full/image-01-full.jpg">
+  //                                           <span class="overlay-img"></span>
+  //                                         <span class="overlay-img-thumb font-icon-plus"></span>
+  //                                     </a>
+  //                                     <!-- Thumb Image and Description -->
+  //                                     <img src="_include/img/work/thumbs/image-01.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+  //                                 </li>
+  //                                   <!-- End Item Project -->
+
+  //                                               <!-- Item Project and Filter Name -->
+  //                                   <li class="item-thumbs span3 design">
+  //                                       <!-- Fancybox - Gallery Enabled - Title - Full Image -->
+  //                                       <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="The Office" href="_include/img/work/full/image-02-full.jpg">
+  //                                           <span class="overlay-img"></span>
+  //                                         <span class="overlay-img-thumb font-icon-plus"></span>
+  //                                     </a>
+  //                                     <!-- Thumb Image and Description -->
+  //                                     <img src="_include/img/work/thumbs/image-02.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+  //                                 </li>
+  //                                   <!-- End Item Project -->
+
+  //                                               <!-- Item Project and Filter Name -->
+  //                                   <li class="item-thumbs span3 photography">
+  //                                       <!-- Fancybox - Gallery Enabled - Title - Full Image -->
+  //                                       <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="The Mountains" href="_include/img/work/full/image-03-full.jpg">
+  //                                           <span class="overlay-img"></span>
+  //                                         <span class="overlay-img-thumb font-icon-plus"></span>
+  //                                     </a>
+  //                                     <!-- Thumb Image and Description -->
+  //                                     <img src="_include/img/work/thumbs/image-03.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+  //                                 </li>
+  //                                   <!-- End Item Project -->
+
+  //                                               <!-- Item Project and Filter Name -->
+  //                                   <li class="item-thumbs span3 video">
+  //                                       <!-- Fancybox Media - Gallery Enabled - Title - Link to Video -->
+  //                                       <a class="hover-wrap fancybox-media" data-fancybox-group="video" title="Video Content From Vimeo" href="http://vimeo.com/51460511">
+  //                                           <span class="overlay-img"></span>
+  //                                         <span class="overlay-img-thumb font-icon-plus"></span>
+  //                                     </a>
+  //                                     <!-- Thumb Image and Description -->
+  //                                     <img src="_include/img/work/thumbs/image-08.jpg" alt="Video">
+  //                                 </li>
+  //                                   <!-- End Item Project -->
+
+  //                                               <!-- Item Project and Filter Name -->
+  //                                   <li class="item-thumbs span3 photography">
+  //                                       <!-- Fancybox - Gallery Enabled - Title - Full Image -->
+  //                                       <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="The Sea" href="_include/img/work/full/image-04-full.jpg">
+  //                                           <span class="overlay-img"></span>
+  //                                         <span class="overlay-img-thumb font-icon-plus"></span>
+  //                                     </a>
+  //                                     <!-- Thumb Image and Description -->
+  //                                     <img src="_include/img/work/thumbs/image-04.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+  //                                 </li>
+  //                                   <!-- End Item Project -->
+
+  //                                               <!-- Item Project and Filter Name -->
+  //                                   <li class="item-thumbs span3 photography">
+  //                                       <!-- Fancybox - Gallery Enabled - Title - Full Image -->
+  //                                       <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Clouds" href="_include/img/work/full/image-05-full.jpg">
+  //                                           <span class="overlay-img"></span>
+  //                                         <span class="overlay-img-thumb font-icon-plus"></span>
+  //                                     </a>
+  //                                     <!-- Thumb Image and Description -->
+  //                                     <img src="_include/img/work/thumbs/image-05.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+  //                                 </li>
+  //                                   <!-- End Item Project -->
+
+  //                                               <!-- Item Project and Filter Name -->
+  //                                   <li class="item-thumbs span3 video">
+  //                                       <!-- Fancybox Media - Gallery Enabled - Title - Link to Video -->
+  //                                       <a class="hover-wrap fancybox-media" data-fancybox-group="video" title="Video Content From Vimeo" href="http://vimeo.com/50834315">
+  //                                           <span class="overlay-img"></span>
+  //                                         <span class="overlay-img-thumb font-icon-plus"></span>
+  //                                     </a>
+  //                                     <!-- Thumb Image and Description -->
+  //                                     <img src="_include/img/work/thumbs/image-09.jpg" alt="Video">
+  //                                 </li>
+  //                                   <!-- End Item Project -->
+
+  //                                               <!-- Item Project and Filter Name -->
+  //                                   <li class="item-thumbs span3 design">
+  //                                       <!-- Fancybox - Gallery Enabled - Title - Full Image -->
+  //                                       <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="The Dark" href="_include/img/work/full/image-06-full.jpg">
+  //                                           <span class="overlay-img"></span>
+  //                                         <span class="overlay-img-thumb font-icon-plus"></span>
+  //                                     </a>
+  //                                     <!-- Thumb Image and Description -->
+  //                                     <img src="_include/img/work/thumbs/image-06.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+  //                                 </li>
+  //                                   <!-- End Item Project -->
+
+  //                                               <!-- Item Project and Filter Name -->
+  //                                   <li class="item-thumbs span3 design">
+  //                                       <!-- Fancybox - Gallery Enabled - Title - Full Image -->
+  //                                       <a class="hover-wrap fancybox" data-fancybox-group="gallery" title="The Beach" href="_include/img/work/full/image-07-full.jpg">
+  //                                           <span class="overlay-img"></span>
+  //                                         <span class="overlay-img-thumb font-icon-plus"></span>
+  //                                     </a>
+  //                                     <!-- Thumb Image and Description -->
+  //                                     <img src="_include/img/work/thumbs/image-07.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+  //                                 </li>
+  //                                   <!-- End Item Project -->
+  //                               </ul>
+  //                         </section>
+
+  // owl.on('mousewheel', '.owl-stage', function (e) {
+  //     if (e.deltaY>0) {
+  //         owl.trigger('next.owl');
+  //     } else {
+  //         owl.trigger('prev.owl');
+  //     }
+  //     e.preventDefault();
+  // });
 })(jQuery);
